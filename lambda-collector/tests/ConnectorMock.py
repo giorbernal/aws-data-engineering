c53831f553e05f574c1c0f69b50109de96e2aa25
Jorge Bernal Chaves <restless_project@hotmail.es>
2023-04-16 20:03:31 +0200

    adjusting test file

--- lambda-collector/tests/ConnectorMock.py
+++ lambda-collector/tests/ConnectorMock.py
@@ -8,10 +8,10 @@ from definitions import ROOT_DIR
 from lambda_collector.data.ConnectorInterface import ConnectorInterface
 from pandas.testing import assert_frame_equal
 
-TEST_RAW_FILE = '/'.join([ROOT_DIR,'tests/datasets/sample.csv'])
-TEST_REF_STATIONS_FILE = '/'.join([ROOT_DIR,'../refs/stations.csv'])
-TEST_REF_PARAMS_FILE = '/'.join([ROOT_DIR,'../refs/params.csv'])
-TEST_S3_DATA = '/'.join([ROOT_DIR,'tests/datasets/s3.csv'])
+TEST_RAW_FILE = '/'.join([ROOT_DIR, 'tests/datasets/sample.csv'])
+TEST_REF_STATIONS_FILE = '/'.join([ROOT_DIR, '../refs/stations.csv'])
+TEST_REF_PARAMS_FILE = '/'.join([ROOT_DIR, '../refs/params.csv'])
+TEST_S3_DATA = '/'.join([ROOT_DIR, 'tests/datasets/s3.csv'])
 
 
 class ConnectorMock(ConnectorInterface):
@@ -37,5 +37,3 @@ class ConnectorMock(ConnectorInterface):
 
     def save_dynamo_data(self, df):
         pass
-
-
