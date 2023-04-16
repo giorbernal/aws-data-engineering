0f2e6c83cfa194f61df10000bbe453f3bcc95c48
Jorge Bernal Chaves <restless_project@hotmail.es>
2023-04-16 20:01:16 +0200

    processing S3 file

--- lambda-collector/lambda_collector/data/DataExtractor.py
+++ lambda-collector/lambda_collector/data/DataExtractor.py
@@ -1,3 +1,32 @@
+from typing import Callable, Any, Generator
+import pandas as pd
+import numpy as np
+
+
+def fit_stations(df):
+    df.columns = ['ESTACION', 'N_ESTACION']
+    df['ESTACION'] = df[['ESTACION']].apply(lambda x: int(str(x[0])[-3:]), axis=1)
+    return df
+
+
+def fit_parameters(df):
+    df.columns = ['MAGNITUD', 'N_MAGNITUD', 'UNIT']
+    return df
+
+
+def get_value_columns():
+    flat_map: Callable[[Any, Any], Generator[Any, Any, None]] = lambda f, xs: (y for ys in xs for y in f(ys))
+    columns = []
+    for i in flat_map(lambda x: x, [[''.join(['H', str(x).zfill(2)]), ''.join(['V', str(x).zfill(2)])] for x in list(range(1, 25))]):
+        columns.append(i)
+    return columns
+
+
+def avg_calculation(x):
+    data = [np.NaN if x[i+1] == 'N' else x[i] for i in pd.Series(list(range(0, 48, 2)))]
+    return pd.Series(data).mean()
+
+
 class DataExtractor:
 
     def __init__(self, ci):
@@ -7,8 +36,8 @@ class DataExtractor:
         try:
             # Read Data
             raw_df = self.ci.get_raw_data()
-            ref_stations = self.__fit_stations__(self.ci.get_ref_stations())
-            ref_parameters = self.__fit_parameters__(self.ci.get_ref_parameters())
+            ref_stations = fit_stations(self.ci.get_ref_stations())
+            ref_parameters = fit_parameters(self.ci.get_ref_parameters())
 
             # Join raw and reference info
             joined_df = raw_df\
@@ -23,19 +52,10 @@ class DataExtractor:
         except Exception as e:
             print(str.join(': ', ['Error while processing data', e]))
 
-    def __fit_stations__(self, df):
-        df.columns = ['ESTACION', 'N_ESTACION']
-        df['ESTACION'] = df[['ESTACION']].apply(lambda x: int(str(x[0])[-3:]), axis=1)
-        return df
-
-    def __fit_parameters__(self, df):
-        df.columns = ['MAGNITUD', 'N_MAGNITUD', 'UNIT']
-        return df
-
     def __adapt_s3_data__(self, df):
-        # TODO
-        agg_df = df
-        self.ci.save_s3_data(agg_df)
+        value_columns = get_value_columns()
+        df['AVG'] = df[value_columns].apply(avg_calculation, axis=1)
+        self.ci.save_s3_data(df.drop(columns=value_columns))
 
     def __adapt_dynamo_data__(self, df):
         # TODO
--- lambda-collector/tests/ConnectorMock.py
+++ lambda-collector/tests/ConnectorMock.py
@@ -6,10 +6,12 @@ sys.path.insert(0, os.getcwd())
 
 from definitions import ROOT_DIR
 from lambda_collector.data.ConnectorInterface import ConnectorInterface
+from pandas.testing import assert_frame_equal
 
 TEST_RAW_FILE = '/'.join([ROOT_DIR,'tests/datasets/sample.csv'])
 TEST_REF_STATIONS_FILE = '/'.join([ROOT_DIR,'../refs/stations.csv'])
 TEST_REF_PARAMS_FILE = '/'.join([ROOT_DIR,'../refs/params.csv'])
+TEST_S3_DATA = '/'.join([ROOT_DIR,'tests/datasets/s3.csv'])
 
 
 class ConnectorMock(ConnectorInterface):
@@ -28,4 +30,12 @@ class ConnectorMock(ConnectorInterface):
     def get_ref_parameters(self):
         df = pd.read_csv(TEST_REF_PARAMS_FILE, sep=';', header=None)
         return df
+
+    def save_s3_data(self, df):
+        expected_df = pd.read_csv(TEST_S3_DATA, sep=';')
+        assert_frame_equal(expected_df, df)
+
+    def save_dynamo_data(self, df):
+        pass
+
 
