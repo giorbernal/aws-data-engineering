87d4d6f4b21b0011005ca86e3b84ba7bd4a8b18a
Jorge Bernal Chaves <restless_project@hotmail.es>
2023-05-07 20:58:10 +0200

    incluiding scheduling in function scripts

--- lambda-collector/scripts/create-function.sh
+++ lambda-collector/scripts/create-function.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
-aws lambda create-function --region ${AWS_REGION} --function-name de-collection \
+COLLECTOR_FUNCTION_NAME=de-collector
+
+# Creating the lambda to collect data
+aws lambda create-function --region ${AWS_REGION} --function-name ${COLLECTOR_FUNCTION_NAME} \
     --package-type Image  \
     --code ImageUri=${AWS_USER}.dkr.ecr.${AWS_REGION}.amazonaws.com/aws-de-lambda-collector:latest \
     --role arn:aws:iam::${AWS_USER}:role/de-lambda-collector \
     --timeout 600
+
+# Creating the EventBridge rule to schedule the lambda
+aws events put-rule --name "de-collector-schedule" --schedule-expression "cron(0 0 * * ? *)"
+
+# Attach a the lambda as a target of the event
+aws events put-targets --rule "de-collector-schedule" --targets "Id"="1","Arn"="arn:aws:lambda:${AWS_REGION}:${AWS_USER}:function:de-collector"
+
+# Set resource permissions to the lambda function to allow the events access to the lambda
+aws lambda add-permission \
+    --function-name "${COLLECTOR_FUNCTION_NAME}" \
+    --statement-id "AWSEvents_de-collector-schedule" \
+    --action "lambda:InvokeFunction" \
+    --principal "events.amazonaws.com" \
+    --source-arn "arn:aws:events:${AWS_REGION}:${AWS_USER}:rule/de-collector-schedule"
--- lambda-collector/scripts/delete-function.sh
+++ lambda-collector/scripts/delete-function.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-aws lambda delete-function --region ${AWS_REGION} --function-name de-collection
+# Let's remove the EventBridge target
+aws events remove-targets --rule "de-collector-schedule" --ids "1"
+
+# Let's remove the rule
+aws events delete-rule --name "de-collector-schedule"
+
+# Let's remove the function
+aws lambda delete-function --region ${AWS_REGION} --function-name de-collector
