6e7494099879ac5f265f50aa46f38f17bccecdf0
Jorge Bernal Chaves <restless_project@hotmail.es>
2023-05-08 19:38:33 +0200

    adding IAM management to the lambda

--- lambda-collector/scripts/create-function.sh
+++ lambda-collector/scripts/create-function.sh
@@ -1,6 +1,38 @@
 #!/bin/bash
 
+# Function to link policies to roles
+function put_role_policy {
+  POL_VER=$(aws iam get-policy --policy-arn "arn:aws:iam::$3:policy/$1" --query 'Policy.DefaultVersionId')
+  aws iam get-policy-version --policy-arn "arn:aws:iam::$3:policy/$1" --version-id ${POL_VER:1:2} --query "PolicyVersion.Document" > temp.json
+  aws iam put-role-policy --role-name de-lambda-collector --policy-name $2 --policy-document file://temp.json
+  rm -f temp.json
+}
+
+# ECR Policy (It could not be necessary ??)
+aws iam create-policy \
+    --policy-name "AWSEcrBasic" \
+    --description "Basic Permissions for ECR access for a lambda" \
+    --policy-document "{\"Version\":\"2012-10-17\",\"Statement\":[{\"Sid\":\"VisualEditor0\",\"Effect\":\"Allow\",\"Action\":[\"ecr:SetRepositoryPolicy\",\"ecr:GetRepositoryPolicy\"],\"Resource\":\"arn:aws:ecr:${AWS_REGION}:${AWS_USER}:repository/*\"}]}"
+
+# Let's create our two functions:
+
+# 1) Function to collect data in a scheduled way
+
 COLLECTOR_FUNCTION_NAME=de-collector
+
+# Managing role and permissions for lambda
+aws iam create-policy --policy-name "AmazonDynamoDBWdpBatchWriteItem" \
+    --description "Permission to write just BathWriteItem to my table weather-datapoints" \
+    --policy-document "{\"Version\":\"2012-10-17\",\"Statement\":[{\"Sid\":\"VisualEditor0\",\"Effect\":\"Allow\",\"Action\":\"dynamodb:BatchWriteItem\",\"Resource\":\"arn:aws:dynamodb:${AWS_REGION}:${AWS_USER}:table/weather-datapoints\"}]}"
+aws iam create-role \
+    --role-name de-lambda-collector \
+    --description "Allows Lambda functions to call AWS services on your behalf." \
+    --assume-role-policy-document '{"Version":"2012-10-17","Statement":[{"Effect":"Allow","Principal":{"Service":"lambda.amazonaws.com"},"Action":"sts:AssumeRole"}]}'
+put_role_policy "AWSEcrBasic" "AWSEcrBasic" "${AWS_USER}"
+put_role_policy "AmazonDynamoDBWdpBatchWriteItem" "AmazonDynamoDBWdpBatchWriteItem" "${AWS_USER}"
+put_role_policy "AmazonS3FullAccess" "AmazonS3FullAccess" "aws"
+put_role_policy "service-role/AWSLambdaBasicExecutionRole" "AWSLambdaBasicExecutionRole" "aws"
+
 
 # Creating the lambda to collect data
 aws lambda create-function --region ${AWS_REGION} --function-name ${COLLECTOR_FUNCTION_NAME} \
@@ -22,3 +54,7 @@ aws lambda add-permission \
     --action "lambda:InvokeFunction" \
     --principal "events.amazonaws.com" \
     --source-arn "arn:aws:events:${AWS_REGION}:${AWS_USER}:rule/de-collector-schedule"
+
+
+# 2) Function to access data by an API Gateway
+# TODO
--- lambda-collector/scripts/delete-function.sh
+++ lambda-collector/scripts/delete-function.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Let's delete our two functions:
+
+# 1) Function to collect data in a scheduled way
+
 COLLECTOR_FUNCTION_NAME=de-collector
 
 # Let's remove the EventBridge target
@@ -10,3 +14,18 @@ aws events delete-rule --name "de-collector-schedule"
 
 # Let's remove the function
 aws lambda delete-function --region ${AWS_REGION} --function-name ${COLLECTOR_FUNCTION_NAME}
+
+# Let's delete role and ad-hoc policies
+aws iam delete-role-policy --role-name de-lambda-collector --policy-name AmazonDynamoDBWdpBatchWriteItem
+aws iam delete-role-policy --role-name de-lambda-collector --policy-name AWSEcrBasic
+aws iam delete-role-policy --role-name de-lambda-collector --policy-name AmazonS3FullAccess
+aws iam delete-role-policy --role-name de-lambda-collector --policy-name AWSLambdaBasicExecutionRole
+aws iam delete-role --role-name de-lambda-collector
+aws iam delete-policy --policy-arn "arn:aws:iam::${AWS_USER}:policy/AmazonDynamoDBWdpBatchWriteItem"
+
+
+# 2) Function to access data by an API Gateway
+# TODO
+
+# Let's delete general policy for
+aws iam delete-policy --policy-arn "arn:aws:iam::${AWS_USER}:policy/AWSEcrBasic"
